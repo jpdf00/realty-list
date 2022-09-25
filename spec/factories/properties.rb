0d06010a2bc1ded44e376e5feb4c4632aba1401b
João Paulo Dias França <j.pdf00@gmail.com>
2022-09-25 15:49:28 -0300

    Adds rubocop linter

--- spec/factories/properties.rb
+++ spec/factories/properties.rb
@@ -2,9 +2,12 @@ FactoryBot.define do
   factory :property do
     name { Faker::Lorem.sentence(word_count: 3, supplemental: false, random_words_to_add: 0) }
     after(:create) do |property|
-      property.photos.attach(io: File.open(Rails.root.join('spec/fixtures/images/cat_1.jpg')), filename: 'cat_1.jpg', content_type: 'image/jpeg')
-      property.photos.attach(io: File.open(Rails.root.join('spec/fixtures/images/cat_2.jpg')), filename: 'cat_2.jpg', content_type: 'image/jpeg')
-      property.photos.attach(io: File.open(Rails.root.join('spec/fixtures/images/cat_3.jpg')), filename: 'cat_3.jpg', content_type: 'image/jpeg')
+      property.photos.attach(io: File.open(Rails.root.join('spec/fixtures/images/cat_1.jpg')), filename: 'cat_1.jpg',
+                             content_type: 'image/jpeg')
+      property.photos.attach(io: File.open(Rails.root.join('spec/fixtures/images/cat_2.jpg')), filename: 'cat_2.jpg',
+                             content_type: 'image/jpeg')
+      property.photos.attach(io: File.open(Rails.root.join('spec/fixtures/images/cat_3.jpg')), filename: 'cat_3.jpg',
+                             content_type: 'image/jpeg')
     end
   end
 end

1c4d40e6fb5ef6049ecb648cfce8ee28b83b78f4
João Paulo Dias França <j.pdf00@gmail.com>
2022-09-25 08:01:17 -0300

    Adds tests

--- spec/views/properties/index.html.erb_spec.rb
+++ spec/views/properties/index.html.erb_spec.rb
@@ -4,10 +4,20 @@ RSpec.describe "properties/index", type: :view do
   before(:each) do
     assign(:properties, [
       Property.create!(
-        name: "Name"
+        name: "Name",
+        photos: [
+          {io: File.open(Rails.root.join('spec/fixtures/images/cat_1.jpg')), filename: 'cat_1.jpg', content_type: 'image/jpeg'},
+          {io: File.open(Rails.root.join('spec/fixtures/images/cat_2.jpg')), filename: 'cat_2.jpg', content_type: 'image/jpeg'},
+          {io: File.open(Rails.root.join('spec/fixtures/images/cat_3.jpg')), filename: 'cat_3.jpg', content_type: 'image/jpeg'}
+        ]
       ),
       Property.create!(
-        name: "Name"
+        name: "Name",
+        photos: [
+          {io: File.open(Rails.root.join('spec/fixtures/images/cat_1.jpg')), filename: 'cat_1.jpg', content_type: 'image/jpeg'},
+          {io: File.open(Rails.root.join('spec/fixtures/images/cat_2.jpg')), filename: 'cat_2.jpg', content_type: 'image/jpeg'},
+          {io: File.open(Rails.root.join('spec/fixtures/images/cat_3.jpg')), filename: 'cat_3.jpg', content_type: 'image/jpeg'}
+        ]
       )
     ])
   end
--- spec/views/properties/show.html.erb_spec.rb
+++ spec/views/properties/show.html.erb_spec.rb
@@ -3,7 +3,12 @@ require 'rails_helper'
 RSpec.describe "properties/show", type: :view do
   before(:each) do
     @property = assign(:property, Property.create!(
-      name: "Name"
+      name: "Name",
+      photos: [
+        {io: File.open(Rails.root.join('spec/fixtures/images/cat_1.jpg')), filename: 'cat_1.jpg', content_type: 'image/jpeg'},
+        {io: File.open(Rails.root.join('spec/fixtures/images/cat_2.jpg')), filename: 'cat_2.jpg', content_type: 'image/jpeg'},
+        {io: File.open(Rails.root.join('spec/fixtures/images/cat_3.jpg')), filename: 'cat_3.jpg', content_type: 'image/jpeg'}
+      ]
     ))
   end
 
